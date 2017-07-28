e2254f116a04d5834d08820c52bfa7da5685b077
Vigneshwaran Rajendran <vigneshwaran.rajendran@tnqmail.com>
2017-07-28 14:33:50 +0530

    comments added

--- ApachePoiColumnData.java
+++ ApachePoiColumnData.java
@@ -23,24 +23,28 @@ public class ApachePoiColumnData {
 	public static void main(String[] args) {
 			
 		try{ 
-			 File src=new File("/home/vigneshwaran/Desktop/test/test.xlsx");		 
-			 FileInputStream inputStream = new FileInputStream(src);		 
-			 Workbook workbook = new XSSFWorkbook(inputStream);
-			 org.apache.poi.ss.usermodel.Sheet firstSheet = workbook.getSheetAt(0);
-			 Row firstRow = firstSheet.getRow(firstSheet.getFirstRowNum());
-			 Integer noOfColumns = new Integer(firstRow.getLastCellNum());
-			 Integer noOfRows = new Integer(firstSheet.getLastRowNum());
-			 
-			 Map<Integer,List> data = new HashMap<>();
-			 
-			 for(int i = 0 ; i < noOfColumns ; i++) {
-				 List listData = new ArrayList<>();
-				 for(Row row : firstSheet) {
-					 Cell cell = row.getCell(i);
-					 listData.add(cell.getStringCellValue());					 
-				 }
-				 data.put(i, listData);
-			 }
+			// Excel File initialize
+			File src=new File("/test.xlsx");		 
+			FileInputStream inputStream = new FileInputStream(src);		 
+			Workbook workbook = new XSSFWorkbook(inputStream);
+			org.apache.poi.ss.usermodel.Sheet firstSheet = workbook.getSheetAt(0);
+			Row firstRow = firstSheet.getRow(firstSheet.getFirstRowNum());
+			Integer noOfColumns = new Integer(firstRow.getLastCellNum()); // Get No of columns
+			Integer noOfRows = new Integer(firstSheet.getLastRowNum()); // Get No of Rows
+			// Empty Map to store the data from excel
+			Map<Integer,List> data = new HashMap<>();
+			// Loop through the each columns and store its value to the map - data
+			for(int i = 0 ; i < noOfColumns ; i++) {
+				List listData = new ArrayList<>();
+				for(Row row : firstSheet) {
+				 Cell cell = row.getCell(i);
+				 listData.add(cell.getStringCellValue());
+				}
+				// Insert values to the map column wise
+				data.put(i, listData);
+				// Print the values through sysout
+				System.out.println(data.toString());
+			}
 			
 		}
 		 catch(Exception ex) {
